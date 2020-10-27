d191bff87924f1efd0cb911d65d70fd435eb8884
sivasaimulka <72981184+sivasaimulka@users.noreply.github.com>
2020-10-27 11:40:26 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,41 @@
+
+public class Employee {
+	private int id;
+	private String name;
+	private double salary;
+	private double hra;
+	private double medical;
+	private double pf;
+	private double pt;
+	private double net_salary;
+	private double gross_salary;
+	public Employee() {
+		this.pt = 200;
+		this.medical = 5000;
+	}
+	public Employee(int id, String name,int salary) {
+		this();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+		this.hra = 0.5*this.salary;
+		this.pf = 0.12*this.salary;
+	}
+	public void details() {
+		System.out.println("Employee ID : "+this.id);
+		System.out.println("Name : "+this.name);
+		System.out.println("Base Salary : "+this.salary);
+	}
+	public double calcGrossSalary() {
+		this.gross_salary = this.salary+this.hra+this.medical;
+		return this.gross_salary;
+	}
+	public double calcNetSalary() {
+		this.net_salary = this.gross_salary - this.pf - this.pt;
+		return this.net_salary;
+	}
+	public void salary_structure() {
+		System.out.println("Gross Salary : "+this.calcGrossSalary());
+		System.out.println("Net Salary : "+this.calcNetSalary());
+	}
+}
---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+	public static void main(String[] args) {
+		Employee e1 = new Employee(10644,"Lucifer",120000);
+		e1.details();
+		e1.salary_structure();
+	}
+}
+
