99fd988dc28bd7855c78a6b6e054519bf2ee8567
B Tirumala <52728214+tirumalab@users.noreply.github.com>
2020-10-27 11:31:31 +0530

    Day 2 assignment by B Tirumala
    - includes Employee details assignment

--- Employee.java
+++ Employee.java
@@ -1,54 +1,27 @@
-package day2;
 public class Employee {
 	public int empid;
 	public String ename;
-	public double BASIC_SALARY;
-	public double HRA;
-	public double medical;
-	public double pf;
-	public double pt;
-	public double NET_SALARY;
-	public double GROSS_SALARY;
-	
-	//public static String company="HSBC";
+	public double basicSalary, HRA, medical, pf, pt, netSalary, grossSalary;
+	public static String comp = "abc";
 
-	/*
-	 * public Employee(int empid, String ename, int salary) { this();
-	 * System.out.println("PARAMETERIZED CONSTR"); this.empid = empid; this.ename =
-	 * ename; this.salary = salary; }
-	 */
-	public Employee()
-	{
-		System.out.println("Default Constr");
+	public Employee() {
+
 	}
 
-	public Employee(int empid, String ename, double bASIC_SALARY, double medical) {
-		super();
-		this.empid = empid;
-		this.ename = ename;
-		BASIC_SALARY = bASIC_SALARY;
+	public Employee(int id, String name, int sal, int medical) {
+		this.empid = id;
+		this.ename = name;
+		this.basicSalary = sal;
+		this.HRA = 0.5 * sal;
+		this.pf = 0.12 * sal;
+		this.pt = 200;
 		this.medical = medical;
-		HRA=.5*BASIC_SALARY ;
-		pf=.12*BASIC_SALARY;
-		pt=200;
-		GROSS_SALARY=BASIC_SALARY+HRA+medical;
-		NET_SALARY=GROSS_SALARY-(pt+pf);
+		this.grossSalary = this.basicSalary + this.HRA + this.medical;
+		this.netSalary = this.grossSalary - this.pt - this.pf;
 	}
 
-	@Override
-	public String toString() {
-		return "Employee [empid=" + empid + ", ename=" + ename + ", BASIC_SALARY=" + BASIC_SALARY + ", HRA=" + HRA
-				+ ", medical=" + medical + ", pf=" + pf + ", pt=" + pt + ", NET_SALARY=" + NET_SALARY
-				+ ", GROSS_SALARY=" + GROSS_SALARY + "]";
+	public void printEmpSalaries() {
+		System.out.println("gross salary:" + this.grossSalary);
+		System.out.println("net salary:" + this.netSalary);
 	}
-	
-	public void displayEmpDetails()
-	{
-		System.out.println(this.toString());
-	}
-
-	/*
-	 * public void printEmp() { System.out.println("ID "+ empid);
-	 * System.out.println("name "+ ename); System.out.println("salary "+ salary); }
-	 */	
 }
