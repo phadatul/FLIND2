3d1a4bde955f272d0cf48d6e02f6d2b21cffe914
AnuragMittal98 <49536242+AnuragMittal98@users.noreply.github.com>
2020-10-27 11:47:34 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,9 @@
+
+public class Demo {
+	public static void main(String[] args) {
+		Employee e = new Employee(45120195, "Anurag Mittal" ,1500000 , 20000);
+		e.print();
+		//Employee f = new Employee(1234 , "AAA" , 4321);
+		//f.print();		
+	}
+}
--- Employee.java
+++ Employee.java
@@ -1,39 +1,36 @@
 
 public class Employee {
-	private int empid;
-	private String ename;
-	private double basic_salary;
-	private double HRA;
-	private double medical;
-	private double PF;
-	private double PT;
-	private double net_salary;
-	private double gross_salary;
+	public int empid;
+	public String ename;
+	public double bsal,hra,medical,pf,pt=200,nsal,gsal;
+	 ;
+	
 	public Employee()
 	{
-		System.out.println("Default Constructor");
-	}
-
-	public Employee (int empid, String ename, double basic_salary,double medical)
-	{
-		this.empid=empid;
-		this.ename=ename;
-		this.basic_salary=basic_salary;
-		this.HRA=0.5*basic_salary;
-		this.PF=0.12*basic_salary;
-		this.PT=1200;
-		this.gross_salary=basic_salary+HRA+medical;
-		this.net_salary= gross_salary -(PT+PF);
 		
 	}
-	
-	public void printEmp()
-	{
-		
-		System.out.println("ID:"+empid);
-		System.out.println("Name:"+ename);
-		System.out.println("Gross Salary:"+gross_salary);
-		System.out.println("Net Salary:"+net_salary);
+	public Employee(int empid , String ename , int bsal , int medical)
+	{	
 
+		this.empid = empid;
+		this.ename = ename;
+		this.bsal = bsal;
+		this.medical = medical;
+		hra = 0.5 * bsal;
+		pf = 0.12 * bsal;
+		gsal = bsal + hra + medical;
+		nsal = gsal - (pt+pf);
+	}
+	public void print()
+	{
+		System.out.println("ID : "+empid);
+		System.out.println("Employee Name : "+ename);
+		System.out.println("Basic Salary : "+bsal);
+		System.out.println("HRA : "+hra);
+		System.out.println("Medical : "+medical);
+		System.out.println("PF : "+pf);
+		System.out.println("PT : "+pt);
+		System.out.println("Net Salary : "+nsal);
+		System.out.println("Gross Salary : "+gsal);
 	}
 }
