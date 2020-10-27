261819f43c4ee4332497bdf4ea6ee5041e26a355
shreyasbetal <betalshreyas@gmail.com>
2020-10-27 11:53:40 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -3,9 +3,12 @@ public class Calculator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Employee e1 = new Employee(45, "Abc", 600000, 200000);
-		e1.showgross();
-		e1.shownet();
+		Employee e1 = new Employee("Abc", 600000, 200000);
+		e1.showdetails();
+		Employee e2 = new Employee("Abcdef", 500000, 100000);
+		e2.showdetails();
+//		e1.showgross();
+//		e1.shownet();
 	}
 
 }
