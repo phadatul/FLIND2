2b42d750890004303443c81a17725f1067e66697
aarushi03 <suzy20159@gmail.com>
2020-10-27 16:01:36 +0530

    Day1 work

---
+++ Day1HSBC/src/Calculator.java
@@ -0,0 +1,35 @@
+
+public class Calculator {
+	
+	public Calculator() {
+		
+	}
+	
+	public void hello() {
+		
+		System.out.println("Hello World");
+	
+	}
+	
+	public int add(int a, int b) {
+		
+		return a + b;
+		
+	}
+	
+	public int add(byte a, byte b) {
+		
+		return a + b;
+		
+	}
+	
+	public int add(short a, short b) {
+		
+		return a + b;
+	}
+	
+	public long add(long a, long b) {
+		
+		return a + b;
+	}
+}
---
+++ Day1HSBC/src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		
+		Calculator c= new Calculator();
+		
+		c.hello();
+		System.out.println(c.add(1,1));
+	}
+	
+}
