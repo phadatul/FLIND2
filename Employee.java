1e564766309797d0605fb941428fc0cfff94a686
abhas2012 <abhas2012@gmail.com>
2020-10-27 11:35:51 +0530

    Day2 employee assignment by Abhas Bhatia

--- Employee.java
+++ Employee.java
@@ -1,27 +1,39 @@
+
 public class Employee {
-	public int empid;
-	public String ename;
-	public double basicSalary, HRA, medical, pf, pt, netSalary, grossSalary;
-	public static String comp = "abc";
-
-	public Employee() {
-
+	private int empid;
+	private String ename;
+	private double basic_salary;
+	private double HRA;
+	private double medical;
+	private double PF;
+	private double PT;
+	private double net_salary;
+	private double gross_salary;
+	public Employee()
+	{
+		System.out.println("Default Constructor");
 	}
 
-	public Employee(int id, String name, int sal, int medical) {
-		this.empid = id;
-		this.ename = name;
-		this.basicSalary = sal;
-		this.HRA = 0.5 * sal;
-		this.pf = 0.12 * sal;
-		this.pt = 200;
-		this.medical = medical;
-		this.grossSalary = this.basicSalary + this.HRA + this.medical;
-		this.netSalary = this.grossSalary - this.pt - this.pf;
+	public Employee (int empid, String ename, double basic_salary,double medical)
+	{
+		this.empid=empid;
+		this.ename=ename;
+		this.basic_salary=basic_salary;
+		this.HRA=0.5*basic_salary;
+		this.PF=0.12*basic_salary;
+		this.PT=1200;
+		this.gross_salary=basic_salary+HRA+medical;
+		this.net_salary= gross_salary -(PT+PF);
+		
 	}
+	
+	public void printEmp()
+	{
+		
+		System.out.println("ID:"+empid);
+		System.out.println("Name:"+ename);
+		System.out.println("Gross Salary:"+gross_salary);
+		System.out.println("Net Salary:"+net_salary);
 
-	public void printEmpSalaries() {
-		System.out.println("gross salary:" + this.grossSalary);
-		System.out.println("net salary:" + this.netSalary);
 	}
 }
