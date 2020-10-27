cd20dd1f8b1470a26a39d9e5caed90c7fce2a34b
Raj mani <35173152+rajmani10raj@users.noreply.github.com>
2020-10-27 12:34:56 +0530

    Create Hello.java

---
+++ Hello.java
@@ -0,0 +1,19 @@
+public class Hello {
+
+	public static void main(String[] args)
+	{
+		
+        Employee e=new Employee("abhishek", 1500, 10000);
+		
+		e.printDetails();
+		
+        Employee e2=new Employee("Raj", 1500, 10000);
+		
+		e2.printDetails();
+		
+       Employee e3=new Employee("Mani", 1500, 10000);
+	    	
+		e3.printDetails();
+		
+	}
+}
