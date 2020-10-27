fdbfc49f2ad5e339f816268c059a865b5d8abe33
sivasaimulka <72981184+sivasaimulka@users.noreply.github.com>
2020-10-27 11:52:13 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,11 @@
 public class Main {
 	public static void main(String[] args) {
-		Employee e1 = new Employee(10644,"Lucifer",120000);
+		Employee e1 = new Employee("Lucifer",120000);
+		Employee e2 = new Employee("Jake",150000);
 		e1.details();
 		e1.salary_structure();
+		e2.details();
+		e2.salary_structure();
 	}
 }
 
