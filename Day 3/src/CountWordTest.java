2fbba0f04c343c16460df680b3a1f0ee6bac28cf
GregariousArchon <sharma.kapil996@gmail.com>
2020-10-28 14:53:16 +0530

    countwordassignment

---
+++ Day 3/src/CountWordTest.java
@@ -0,0 +1,29 @@
+
+import java.util.HashMap;
+import java.util.Map;
+ 
+public class CountWordTest {
+    public static void main(String[] args) {
+        CountWordTest countWordTest = new CountWordTest();
+        countWordTest.countWords("hello world how are you where are you what is your name how is your car");
+    }
+ 
+    public void countWords(String input) {
+        Map <String, String> map = new HashMap <String, String> ();
+ 
+        if (input != null) {
+            String[] separatedWords = input.split(" ");
+            for (String str: separatedWords) {
+                if (map.containsKey(str)) {
+                    int count = Integer.parseInt(map.get(str));
+                    map.put(str, String.valueOf(count + 1));
+                } else {
+                    map.put(str, "1");
+                }
+            }
+        }
+ 
+        System.out.println("Count :- " + map);
+    }
+ 
+}
