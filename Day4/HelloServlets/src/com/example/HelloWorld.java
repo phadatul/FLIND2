b07690d7fdee07af97d4a2a1f7935f0fa728b1bf
Anmol Gautam <tarptaeya@gmail.com>
2020-10-29 16:44:46 +0530

    added HelloWorld.java to HelloServlet

---
+++ Day4/HelloServlets/src/com/example/HelloWorld.java
@@ -0,0 +1,38 @@
+package com.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/HelloWorld")
+public class HelloWorld extends HttpServlet {	
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		System.out.println("HelloWorld servlet init called");
+	}
+	
+	@Override
+	public void destroy() {
+		super.destroy();
+		System.out.println("HelloWorld servlet destroy called");
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("HelloWorld servlet doGet called");
+		PrintWriter out = response.getWriter();
+		out.println("Hello World");
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("HelloWorld servlet doPost called");
+	}
+
+}
