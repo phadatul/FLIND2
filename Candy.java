deed0803750d4a2d27da34d6b85b2f216800c88e
AnuragMittal98 <49536242+AnuragMittal98@users.noreply.github.com>
2020-10-29 14:04:58 +0530

    Add files via upload

--- Candy.java
+++ Candy.java
@@ -1,4 +1,23 @@
 
-public class Candy {
+public class Candy extends DessertItem{
+	private double weight , price;
+	
+	public Candy()
+	{
+		
+	}
 
+	public Candy(String name , double weight) {
+		super(name);
+		// TODO Auto-generated constructor stub
+		this.weight = weight ; 
+		this.price = 100;
+	}
+	
+	public double getCost()
+	{
+		return this.weight/1000*this.price;
+	}
+	
+	
 }
--- Icecream.java
+++ Icecream.java
@@ -1,4 +1,21 @@
 
-public class Icecream {
-
+public class Icecream extends DessertItem{
+	private double cost;
+	
+	public Icecream()
+	{
+		
+	}
+	
+	public Icecream(String name)
+	{
+		super(name);
+		this.cost = 10;
+	}
+	
+	public double getCost()
+	{
+		return this.cost;
+	}
+	
 }
