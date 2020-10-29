3e8f72e3f790ce8658ada1781df64d950fa6456a
GregariousArchon <sharma.kapil996@gmail.com>
2020-10-29 17:22:18 +0530

    servletexample

---
+++ trainingProject/src/com/hsbc/servlet/HelloWorld.java
@@ -0,0 +1,57 @@
+package com.hsbc.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet("/HelloWorld")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	  public HelloWorld() {
+	        super();
+	        // TODO Auto-generated constructor stub
+	    }
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+	@Override
+	public void init() throws ServletException {
+		// TODO Auto-generated method stub
+		super.init();
+		System.out.println("init........");
+	}
+  
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+			System.out.println("doGet.....");
+			PrintWriter out = response.getWriter();
+			out.println("THIS IS HELLO WORLD");
+			out.println(request.getParameter("fname"));
+			out.println(request.getParameter("lname"));
+			
+		
+//		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+//	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		doGet(request, response);
+//	}
+
+}
