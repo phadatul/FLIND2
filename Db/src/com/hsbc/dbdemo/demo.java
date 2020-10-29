1d146eba742e7444fb7d03643f9d71eb974b4aed
GregariousArchon <sharma.kapil996@gmail.com>
2020-10-29 14:57:46 +0530

    updated main

--- Db/src/com/hsbc/dbdemo/demo.java
+++ Db/src/com/hsbc/dbdemo/demo.java
@@ -10,27 +10,39 @@ import java.sql.Statement;
 public class demo {
 	public static void main (String []args) {
 		
+//		try {
+//			Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
+//			
+//			Connection con = DriverManager.getConnection("jdbc:derby:kapildb;create=true");
+//			Statement st = con.createStatement();
+////			PreparedStatement ps = con.prepareStatement("insert into employee values(?,?,?)");
+////			ps.setInt(1,id);
+////			ps.setString
+//			
+//			
+//			
+//			ResultSet rs = st.executeQuery("SELECT * FROM employee");
+//			while(rs.next()) {
+//				System.out.println(rs.getInt(1)+" "+rs.getString("name")+" "+rs.getInt(3));
+//			}
+//		} catch (ClassNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+		
+		controller c = new controller();
+		ResultSet rs = c.display();
 		try {
-			Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
-			
-			Connection con = DriverManager.getConnection("jdbc:derby:kapildb;create=true");
-			Statement st = con.createStatement();
-//			PreparedStatement ps = con.prepareStatement("insert into employee values(?,?,?)");
-//			ps.setInt(1,id);
-//			ps.setString
-			
-			
-			
-			ResultSet rs = st.executeQuery("SELECT * FROM employee");
 			while(rs.next()) {
 				System.out.println(rs.getInt(1)+" "+rs.getString("name")+" "+rs.getInt(3));
 			}
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+	
 }
