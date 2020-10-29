3584ff70ae2bd65be02160dc3cc9f04b73a51a8b
shreyasbetal <betalshreyas@gmail.com>
2020-10-29 14:58:40 +0530

    Add files via upload

---
+++ Day1/HelloWorld.java
@@ -0,0 +1,12 @@
+
+public class HelloWorld {
+	public static void main(String []args){
+
+		Vehicle v = new Vehicle();
+		Car c = new Car();
+		Bike b = new Bike();
+		
+		v.move();
+		c.move();
+	}
+}
