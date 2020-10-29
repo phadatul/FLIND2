0f8eda9961ee9af669726af15bffcef02079c1bf
oasmk <66384483+oasmk@users.noreply.github.com>
2020-10-29 17:35:00 +0530

    day 4

---
+++ day4/src/com/hsbc/web/HelloWorld.java
@@ -0,0 +1,29 @@
+package com.hsbc.web;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/HelloWorld")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println("Hello! You entered the following data: ");
+		out.println(request.getParameter("username"));
+		out.println(request.getParameter("password"));
+
+	}
+
+
+}
