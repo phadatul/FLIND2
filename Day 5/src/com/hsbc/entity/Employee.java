59351c3fd6d2461e1cacac2c71c968e53b6c91c7
starkfordge <73517266+starkfordge@users.noreply.github.com>
2020-11-02 17:00:32 +0530

    Add files via upload

---
+++ Day 5/src/com/hsbc/entity/Employee.java
@@ -0,0 +1,77 @@
+package com.hsbc.entity;
+
+public class Employee {
+	
+	private int empid;
+	private String name;
+	private int salary;
+	
+	
+	
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public Employee(int empid, String name, int salary) {
+		super();
+		this.empid = empid;
+		this.name = name;
+		this.salary = salary;
+	}
+
+
+	public int getEmpid() {
+		return empid;
+	}
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+
+
+	@Override
+	public String toString() {
+		return "[empid=" + empid + ", name=" + name + ", salary=" + salary + "]";
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + empid;
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (this.getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (empid != other.empid)
+			return false;
+		return true;
+	}
+	
+	
+	
+
+}
