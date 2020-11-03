b79c9d21c4287b6ce5c2dc66094cfd734c1be132
RIYA SINHA <riya.sinha1998@gmail.com>
2020-11-03 17:32:25 +0530

    Update NoteStore.java

--- Exam_RiyaSinha/NoteStore.java
+++ Exam_RiyaSinha/NoteStore.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
 
 public class NoteStore {
 	
-	public static ArrayList<TextNote> textnoteDatabase;
-	public static ArrayList<TextAndImageNote> textandimagenoteDatabase;
+	public static ArrayList<TextNote> textnoteDatabase; //arraylist to store text notes
+	public static ArrayList<TextAndImageNote> textandimagenoteDatabase; //arraylist to store text and image notes
 	
 	static {
 		textnoteDatabase = new ArrayList<TextNote>();
@@ -17,20 +17,24 @@ public class NoteStore {
 		// TODO Auto-generated constructor stub
 	}
 	
+	//method to store text notes
 	public  void storeNote(String note) {
 		TextNote textnote  = new TextNote(note);
 		textnoteDatabase.add(textnote);
 	}
 	
+	//method to store text and image notes
 	public  void storeNote(String note, String url) {
 		TextAndImageNote textandimagenote = new TextAndImageNote(note, url);
 		textandimagenoteDatabase.add(textandimagenote);
 	}
 	
+	//method to return text notes database
 	public  ArrayList<TextNote> getAllTextNotes() {
 		return textnoteDatabase;
 	}
 	
+	//method to return text and image notes database
 	public  ArrayList<TextAndImageNote> getAllTextAndImageNotes() {
 		return textandimagenoteDatabase;
 	}
