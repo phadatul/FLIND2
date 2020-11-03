5dd8f8d1c402d352756dcb734be95e4acc163883
Anmol Gautam <tarptaeya@gmail.com>
2020-11-03 11:24:11 +0530

    added more methods

--- Day7/MyRestAPI/src/main/java/com/example/helloworldspring/HelloController.java
+++ Day7/MyRestAPI/src/main/java/com/example/helloworldspring/HelloController.java
@@ -1,23 +1,39 @@
 package com.example.helloworldspring;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 	
-	@RequestMapping(value="/", method=RequestMethod.GET)
+	@RequestMapping(value="/")
 	public String welcome() {
 		return "Welcome";
 	}
 	
-	@RequestMapping(value="/greet", method=RequestMethod.GET)
+	@RequestMapping(value="/greet")
 	public String greet(String name) {
 		if (name == null) {
 			name = "World";
 		}
 		
 		return "Hello " + name;
+	}
+	
+	@RequestMapping(value="/get-employee")
+	public Employee getEmployee() {
+		return new Employee(1, "ABCD", 100);
+	}
+	
+	@RequestMapping(value="/list-employees")
+	public List<Employee> listEmployees() {
+		return new ArrayList<Employee>() {{
+			add(new Employee(1, "ABC", 100));
+			add(new Employee(2, "BCD", 105));
+			add(new Employee(3, "CDE", 110));
+		}};
 	}
 }
